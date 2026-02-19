9ad5271062bf14040d92a7db6ffe4e0e5b38de75
Michele Antonio Portulano <michele.antonio.portulano@gmail.com>
2026-02-19 09:40:22 +0100

    libreria con le costanti scientifiche principali

---
+++ const_sci.pas
@@ -0,0 +1,16 @@
+unit const_sci;
+interface
+const
+  c=299792458;
+  G_u=6.67430e-11;
+  h=6.62607e-34;
+  k=1.38064e-23;
+  Na=6.02214e23;
+  R=8.31446;
+  g_e=9.80665;
+  au=1.49597e11;
+  e=2.718281828;
+  phi=1.618033988;
+  atm=101325;
+implementation
+end.
